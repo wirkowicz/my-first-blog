6c551c6dcaa84475daec9fed0b8d861549ecf71f
Piotr W <piotrw678@gmail.com>
2016-05-10 16:00:55 +0200

    kotel

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^kotel', views.kotel, name='kotel'),
     url(r'^$', views.post_list, name='post_list'),
+	
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+	
+def kotel(request):
+    return render(request, 'blog/kotel.html', {})
