a7d903f0c569a1cba0acd7cfb7b099e2af0dddbe
Piotr W <piotrw678@gmail.com>
2016-05-12 14:37:45 +0200

    single post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +11,7 @@ def kotel(request):
 def post_list(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+	
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
